9213aab4ddbfd51f683edefba27b76efe264c816
Dmitriy <dima22022005@gmail.com>
2022-04-17 17:07:52 +0300

    float to int downcast

---
+++ typecasting, ASCII table/downcast.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void) {
+    float initial_num = 0;
+    int changed_num = 0;
+
+    puts("Type a decimal number, please.");
+    scanf("%f", &initial_num);
+
+    changed_num = initial_num;
+
+    printf("Decimal: %f Integer: %d", initial_num, changed_num);
+}
