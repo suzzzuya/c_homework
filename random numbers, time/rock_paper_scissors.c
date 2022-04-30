a52c1f853f78a9df2bf1663c80fb7f1d6fd3ece5
Dmitriy <dima22022005@gmail.com>
2022-04-30 19:52:12 +0300

    refactoring

--- random numbers, time/rock_paper_scissors.c
+++ random numbers, time/rock_paper_scissors.c
@@ -15,47 +15,36 @@ int main() {
     int user_move_int = 0;
     scanf(" %c", &user_move_char);
 
+    int ai_move = rand() % 3 + 1; // random number [1 - 3]
+
     if (user_move_char == 'R' || user_move_char == 'r') {
-        int ai_move = rand() % 3 + 1; // random number [1 - 3]
         user_move_int = 1; // ROCK
-        
-        if (user_move_int == ai_move) {
-            puts("DRAW!");
-        } else if (ai_move == 2) {
-            puts("AI chose: PAPER");
-            puts("You LOSER!");
-        } else {
-            puts("AI chose: Scissors");
-            puts("You WINNER!");
-        }
     } else if (user_move_char == 'P' || user_move_char == 'p') {
-        int ai_move = rand() % 3 + 1; // random number [1 - 3]
         user_move_int = 2; // PAPER
-
-        if (user_move_int == ai_move) {
-            puts("DRAW!");
-        } else if (ai_move == 1) {
-            puts("AI chose: ROCK");
-            puts("You WINNER!");
-        } else {
-            puts("AI chose: Scissors");
-            puts("You LOST!");
-        }
     } else if (user_move_char == 'S' || user_move_char == 's') {
-        int ai_move = rand() % 3 + 1; // random number [1 - 3]
         user_move_int = 3; // SCISSORS
-
-        if (user_move_int == ai_move) {
-            puts("DRAW!");
-        } else if (ai_move == 2) {
-            puts("AI chose: Paper");
-            puts("You WINNER!");
-        } else {
-            puts("AI chose: ROCK");
-            puts("You LOSER!");
-        }
     } else {
         puts("Incorrect input, you LOSER!");
+    }
+
+    if (ai_move == 1){
+        puts("AI chose: Rock");
+    } else if (ai_move == 2) {
+        puts("AI chose: Paper");
+    } else if (ai_move == 3) {
+        puts("AI chose: Scissors");
+    } else {
+        // error
+    }
+
+    if (user_move_int == ai_move) {
+        puts("DRAW!");
+    } else if ((ai_move == 1 && user_move_int == 2) || 
+        (ai_move == 2 && user_move_int == 3) || 
+        (ai_move == 3 && user_move_int == 1) ) {
+        puts("You WINNER!");
+    } else {
+        puts("You LOSER!");
     }
 
     return 0;
