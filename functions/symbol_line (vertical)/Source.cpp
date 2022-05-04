096c3e385cf39a1beecba8b901a2aeba0099a95d
Dmitriy <dima22022005@gmail.com>
2022-05-04 20:37:41 +0300

    output a line with symbol and has is_vertical parameter, if it's value is 1 - places symbols vertically

---
+++ functions/symbol_line (vertical)/Source.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int draw_symbol_line(char symbol, int length, int is_vertical);
+
+int main(void) {
+	char symbol = '*';
+	int length = 5;
+	int is_vertical = 1;
+
+	draw_symbol_line(symbol, length, is_vertical);
+}
+
+int draw_symbol_line(char symbol, int length, int is_vertical) {
+	if (symbol > 32 && symbol != 127 && length > 0) {
+		if (is_vertical > 1 || is_vertical < 1) {
+			for (int i = 0; i < length; i++) {
+				printf("%c", symbol);
+			}
+		}
+		else {
+			for (int i = 0; i < length; i++) {
+				printf("%c", symbol);
+				if (i < length - 1) {
+					printf("\n");
+				}
+			}
+		}
+		return 1;
+	}
+
+	return 0;
+
+}
