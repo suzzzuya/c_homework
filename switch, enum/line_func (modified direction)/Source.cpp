6c5bfa3eea22bf9ce682e6b7af91541fa603e241
Dmitriy <dima22022005@gmail.com>
2022-05-11 16:48:46 +0300

    formatting

--- switch, enum/line_func (modified direction)/Source.cpp
+++ switch, enum/line_func (modified direction)/Source.cpp
@@ -9,22 +9,24 @@ enum Result {
 };
 
 enum Direction {
-	Vertical,
-	Horizontal
+	VERTICAL,
+	HORIZONTAL
 };
 
 Result draw_symbol_line(char symbol, int length, Direction direction);
 
 int main(void) {
-	printf("%d", draw_symbol_line('[', 9, Vertical));
+	printf("%d", draw_symbol_line('[', 9, VERTICAL));
 }
 
 Result draw_symbol_line(char symbol, int length, Direction direction) {
 	if (length <= 0) {
 		return WRONG_LENGTH;
-	} else if (symbol < 32 && symbol != 127) {
+	} 
+	if (symbol < 32 && symbol != 127) {
 		return WRONG_SYMBOL;
-	} else if (direction == Vertical) {
+	} 
+	if (direction == VERTICAL) {
 		for (int i = 0; i < length; i++) {
 			printf("%c\n", symbol);
 		}
