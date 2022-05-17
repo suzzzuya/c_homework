838dfc04ce1c168a9ed82182d6dd529d3c90219a
Dmitriy <dima22022005@gmail.com>
2022-05-18 01:43:51 +0300

    modified atm app. tracks your deposits and withdrawals. it can be shown with [L]ogs menu

---
+++ static arrays/atm/Source.cpp
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void print_menu();
+void print_logs(float arr[], int size);
+
+int main(void) {
+    float balance = 5000;
+    const float LOAN = 1000;
+
+    char user_input = ' ';
+
+    float deposit_amount = 0;
+    float withdraw_amount = 0;
+
+    const float logs_size = 10;
+    int index = 0;
+    float logs_array[10] = { 0 };
+
+    puts("WELCOME TO THE BEST ATM\n");
+
+    while (user_input != 'e' && user_input != 'E') {
+        print_menu();
+        scanf(" %c", &user_input);
+
+        switch (user_input) {
+        case 'D':
+        case 'd':
+            puts("Enter amount, please.");
+            scanf(" %f", &deposit_amount);
+            if (deposit_amount <= 0) {
+                puts("Wrong input.");
+            } else {
+                printf("You have deposited %.2f $\n", deposit_amount);
+                balance = deposit_amount + balance;
+                logs_array[index] = deposit_amount;
+                index++;
+            }
+            break;
+        case 'W':
+        case 'w':
+            puts("Enter amount to withdraw, please.");
+            scanf("%f", &withdraw_amount);
+            if (withdraw_amount <= 0 || withdraw_amount > balance + LOAN) {
+                puts("Wrong input.");
+            } else {
+                printf("You have withdrawn %.2f $\n", withdraw_amount);
+                balance = balance - withdraw_amount;
+                logs_array[index] = -withdraw_amount;
+                index++;
+            }
+            break;
+        case 'L':
+        case 'l':
+            print_logs(logs_array, logs_size);
+            break;
+        case 'B':
+        case 'b':
+            if (balance < 0) {
+                printf("Loan: %.2f\n", -balance);
+            } else {
+                printf("Balance: %.2f\n", balance);
+            }
+            break;
+        }
+    }
+    return 0;
+}
+
+void print_menu() {
+    puts("MENU\n");
+    puts("[D]eposit");
+    puts("[W]ithdraw");
+    puts("[B]alance");
+    puts("[L]ogs");
+    puts("[E]xit");
+}
+
+void print_logs(float arr[], int size) {
+    for (int i = 0; i < size, arr[i] != 0; i++) {
+        printf("%f\n", arr[i]);
+    }
+}
