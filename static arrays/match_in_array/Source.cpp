7f7591fd56f16201b78f3b8d797c981e55bf076c
Dmitriy <dima22022005@gmail.com>
2022-05-17 19:45:33 +0300

    finds a number in array, returning its index

---
+++ static arrays/match_in_array/Source.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int find_num(int arr[], int size, int number);
+
+int main(void) {
+	const int SIZE = 5;
+	int arr[SIZE] = { 4, 6, 1, -75, 3 };
+
+	printf("%d", find_num(arr, SIZE, -75));
+	return 0;
+}
+
+int find_num(int arr[], int size, int number) {
+	for (int i = 0; i < size; i++) {
+		if (number == arr[i]) {
+			return i;
+		}
+	}	return -1;
+}
