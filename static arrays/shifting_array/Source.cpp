d9f454cd0e1b69a21f98913a4e966e1ccaf05e8e
Dmitriy <dima22022005@gmail.com>
2022-05-17 22:49:53 +0300

    adds a num to array and shifts it left

---
+++ static arrays/shifting_array/Source.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void add_num(int arr[], int size, int num);
+void print_arr(int arr[], int size);
+
+int main(void) {
+	const int SIZE = 5;
+	int arr[SIZE] = { 2, 5, 1, 7, 2 };
+
+	add_num(arr, SIZE, -1);
+	return 0;
+}
+
+void add_num(int arr[], int size, int num) {
+	for (int i = 0; i < size - 1; i++) {
+		arr[i] = arr[i + 1];
+	} arr[size - 1] = num;
+	print_arr(arr, size);
+}
+
+void print_arr(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%d\n", arr[i]);
+	}
+}
