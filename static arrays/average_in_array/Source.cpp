696c3bfcbbc591f5fd08e3d136f126c57fe5c0f6
Dmitriy <dima22022005@gmail.com>
2022-05-17 21:46:32 +0300

    return the average of array

---
+++ static arrays/average_in_array/Source.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float arr_avg(int arr[], int size);
+
+int main(void) {
+	const int SIZE = 5;
+	int arr[SIZE] = { -12, 45, 3, 5, 2 };
+
+	printf("%f", arr_avg(arr, SIZE));
+	return 0;
+}
+
+float arr_avg(int arr[], int size) {
+	float sum = 0;
+	float result = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	} result = sum / size;
+	return result;
+}
