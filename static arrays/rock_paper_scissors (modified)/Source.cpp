e892605f9eb8004f2aa674c25228eab6fadbaeda
Dmitriy <dima22022005@gmail.com>
2022-05-20 23:12:35 +0300

    global refactoring

--- static arrays/rock_paper_scissors (modified)/Source.cpp
+++ static arrays/rock_paper_scissors (modified)/Source.cpp
@@ -4,141 +4,172 @@
 #include <stdlib.h>
 #include <time.h>
 
-void print_result(int ai[], int player[], int size, int user_score, int ai_score);
-void check_result(int ai[], int player[], int index, int user_score, int ai_score);
+enum Move {
+    ROCK,
+    PAPER,
+    SCISSORS
+};
+
+enum Result {
+    PLAYER_WON,
+    AI_WON,
+    DRAW
+};
+
+void print_result(int ai[], int player[], int size, int player_score, int ai_score);
+Result check(int player_move, int ai_move);
 
 int main() {
-    srand(time(0) / 4 * 69);
+    srand(time(0));
 
-    puts("Welcome to ROCK-PAPER-SCISSORS\n");
+    puts("Welcome to ROCK-PAPER-SCISSORS");
 
     const int MAX_SCORE = 5;
-    int index = 0;
+    int length = 0;
     int player[100] = { 0 };
     int ai[100] = { 0 };
 
-    int user_score = 0;
+    int player_score = 0;
     int ai_score = 0;
 
-    while (user_score < MAX_SCORE && ai_score < MAX_SCORE) {
-        puts("[R]ock"); // 1
-        puts("[P]aper"); // 2
-        puts("[S]cissors"); // 3
+    while (player_score < MAX_SCORE && ai_score < MAX_SCORE) {
+        printf("\n");
+        puts("[R]ock");
+        puts("[P]aper");
+        puts("[S]cissors");
 
-        char user_move_char = ' ';
-        int user_move_int = 0;
-        scanf(" %c", &user_move_char);
+        char player_move_char = ' ';
+        int player_move = 0;
+        scanf(" %c", &player_move_char);
 
         int ai_move = rand() % 3 + 1; // random number [1 - 3]
 
-        switch (user_move_char) {
+        printf("\n");
+
+        switch (player_move_char) {
         case 'R':
         case 'r':
-            user_move_int = 1;
+            player_move = ROCK;
+            puts("PLAYER chose: ROCK");
             break;
         case 'P':
         case 'p':
-            user_move_int = 2;
+            player_move = PAPER;
+            puts("PLAYER chose: PAPER");
             break;
         case 'S':
         case 's':
-            user_move_int = 3;
+            player_move = SCISSORS;
+            puts("PLAYER chose: SCISSORS");
+            break;
         default:
             puts("Incorrect input.");
             break;
         }
 
-        player[index] = user_move_int;
-
         switch (ai_move) {
-            case 1:
-               puts("AI chose: Rock");
-               break;
-            case 2:
-                puts("AI chose: Paper");
-                break;
-            case 3:
-                puts("AI chose: Scissors");
-                break;
-            default:
-                break;
+        case 1:
+            ai_move = ROCK;
+            puts("AI chose: ROCK");
+            break;
+        case 2:
+            ai_move = PAPER;
+            puts("AI chose: PAPER");
+            break;
+        case 3:
+            ai_move = SCISSORS;
+            puts("AI chose: SCISSORS");
+            break;
         }
 
-        ai[index] = ai_move;
-
-        index++;
-        if (user_move_int == ai_move) {
-            puts("DRAW!");
-        } else if ((ai_move == 1 && user_move_int == 2) ||
-            (ai_move == 2 && user_move_int == 3) ||
-            (ai_move == 3 && user_move_int == 1)) {
-            puts("You WINNER!");
-            user_score++;
-        } else {
+        switch (check(player_move, ai_move)) {
+        case DRAW:
+            puts("DRAW");
+            break;
+        case PLAYER_WON:
+            puts("You won !");
+            player_score++;
+            break;
+        case AI_WON:
+            puts("You lost !");
             ai_score++;
-            puts("You LOSER!\n");
+            break;
         }
-        printf("Your score: %d\tEnemy score: %d\n", user_score, ai_score);
+
+        player[length] = player_move;
+        ai[length] = ai_move;
+
+        length++;
+
+        printf("Your score: %d\tEnemy score: %d\n", player_score, ai_score);
     }
-    print_result(ai, player, index, user_score, ai_score);
+    print_result(ai, player, length, player_score, ai_score);
     return 0;
 }
 
-void check_result(int ai[], int player[], int index, int user_score, int ai_score) {
-    if (player[index] == ai[index]) {
-        puts("DRAW!");
-    } else if ((ai[index] == 'R' || ai[index] == 'r' && player[index] == 'P' || player[index] == 'p') ||
-        (ai[index] == 'P' || ai[index] == 'p' && player[index] == 'S' || player[index] == 's') ||
-        (ai[index] == 'S' || ai[index] == 's' && player[index] == 'R' || player[index] == 'r')) {
-        user_score++;
-        puts("You WINNER!");
+enum Result check(int player_move, int ai_move) {
+    if (player_move == ai_move) {
+        return DRAW;
+    }
+    if ((ai_move == ROCK && player_move == PAPER) ||
+        (ai_move == PAPER && player_move == SCISSORS) ||
+        (ai_move == SCISSORS && player_move == ROCK)) {
+        return PLAYER_WON;
     } else {
-        ai_score++;
-        puts("You LOSER!\n");
+        return AI_WON;
     }
 }
 
-void print_result(int ai[], int player[], int size, int user_score, int ai_score) {
+void print_result(int ai[], int player[], int size, int player_score, int ai_score) {
     printf("\n");
     puts("\tResults\n");
     for (int i = 0; i < size; i++) {
         printf("\tRound %d \n", i + 1);
 
         switch (player[i]) {
-        case 1:
-            printf("Player chose: ROCK");
+        case ROCK:
+            puts("Player chose: ROCK");
             break;
-        case 2:
-            printf("Player chose: PAPER");
+        case PAPER:
+            puts("Player chose: PAPER");
             break;
-        case 3:
-            printf("Player chose: SCISSORS");
+        case SCISSORS:
+            puts("Player chose: SCISSORS");
             break;
         default:
             break;
         }
-        printf("\n");
         switch (ai[i]) {
-            case 1:
-                printf("AI chose: ROCK");
-                break;
-            case 2:
-                printf("AI chose: PAPER");
-                break;
-            case 3:
-                printf("AI chose: SCISSORS");
-                break;
-            default:
-                break;
-        } 
+        case ROCK:
+            puts("AI chose: ROCK");
+            break;
+        case PAPER:
+            puts("AI chose: PAPER");
+            break;
+        case SCISSORS:
+            puts("AI chose: SCISSORS");
+            break;
+        default:
+            break;
+        }
+        switch (check(player[i], ai[i])) {
+        case DRAW:
+            puts("\tDRAW");
+            break;
+        case PLAYER_WON:
+            puts("\tYou won !");
+            break;
+        case AI_WON:
+            puts("\tYou lost !");
+            break;
+        }
         printf("\n");
-        check_result(player, ai, i, user_score, ai_score);
     }
     puts("\tOverall points");
-    printf("PLAYER: %d\n", user_score);
-    printf("AI: %d\n", ai_score);
+    printf("PLAYER: %d\n", player_score);
+    printf("AI: %d", ai_score);
 
-    if (user_score > ai_score) {
+    if (player_score > ai_score) {
         puts("\tYOU WON THE GAME !");
     } else {
         puts("\tYOU LOST THE GAME !");
