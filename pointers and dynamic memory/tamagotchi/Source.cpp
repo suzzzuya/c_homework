6e6523239600185f9823a1ceb84e7a5beac74329
Dmitriy <dima22022005@gmail.com>
2022-07-18 01:21:21 +0300

    add some new features

--- pointers and dynamic memory/tamagotchi/Source.cpp
+++ pointers and dynamic memory/tamagotchi/Source.cpp
@@ -28,15 +28,17 @@ void give_pill(Pet * pet);
 void refresh_status(Pet * pet, int *lastActionTime, bool * hasEaten);
 
 int main(void) {
-	puts("\tTamagotchi\n");
 	for (;pet.health > 0;) {
 		system("cls");
-		puts("MENU\n");
+		puts("\tTamagotchi");
+		puts("watch your pet's life-necessary bars !\n");
+		puts("\tMENU\n");
 		puts("[V]iew status");
 		puts("[F]eed pet");
 		puts("[C]lean up poop");
 		puts("[G]ive pill");
 		printf("\n");
+
 
 		char menu_input = 0;
 		scanf(" %c", &menu_input);
@@ -96,55 +98,101 @@ void go_back_button(char menu_input) {
 	while (menu_input != 'B' && menu_input != 'b') {
 		printf("\n[B]ack\n");
 		scanf(" %c", &menu_input);
+		system("cls");
 	}
 }
 
 void view_status(Pet * pet) {
-	printf("Health: %.2f\nSatiety: %.2f\nPoop level: %.2f", pet->health, pet->satiety, pet->poopLevel);
+	printf("Health: %.2f\nSatiety: %.2f\nPoop level: %.2f\n", pet->health, pet->satiety, pet->poopLevel);
+
 	printf("\n");
+	if (pet->health < 100 || pet->satiety <= 50 || pet->poopLevel >= 75) {
+		puts("\tWARNINGS");
+	}
+	if (pet->health < 100) {
+		puts("Your pet is losing your health ! Give your pet a pill !");
+	}
+	if (pet->satiety <= 50) {
+		puts("Your pet is hungry ! Give your pet a treat !");
+	}
+	if (pet->poopLevel >= 75) {
+		puts("Your pet wants to poop ! Clean up after it !");
+	}
 	time(0);
 }
 
 void feed_pet(Pet * pet, bool * hasEaten) {
-	printf("You have fed your pet !\n");
-	puts("+20 to satiety");
-	pet->satiety += 30;
 	if (pet->satiety >= 100) {
+		puts("pet doesn't want to eat");
 		pet->satiety = maxSatietyValue;
+	} else if (pet->satiety >= 0 && pet->satiety < 100) {
+		printf("You have fed your pet !\n");
+		puts("+25 to satiety");
+		pet->satiety += 25;
+		*hasEaten = true;
+		if (pet->satiety >= 100) {
+			pet->satiety = maxSatietyValue;
+		}
+	} else {
+		pet->satiety = 0;
 	}
-	*hasEaten = true;
+	
 }
 
 void clean_poop(Pet * pet, bool * hasEaten) {
-	printf("You have cleaned up after your pet !\n");
-	puts("poop need restored");
-	pet->poopLevel = 0;
-	*hasEaten = false;
+	if (pet->poopLevel >= 100) {
+		pet->poopLevel = maxPoopLevel;
+	}
+	else if (pet->poopLevel > 0 && pet->poopLevel <= 100) {
+		printf("You have cleaned up after your pet !\n");
+		puts("now poop level is 0");
+		*hasEaten = false;
+		pet->poopLevel = 0;
+	} else {
+		puts("pet doesn't want to poop");
+	}
 }
 
 void give_pill(Pet * pet) {
-	printf("You gave your pet a pill !\n");
-	puts("+30 to health");
-	pet->health += 30;
 	if (pet->health >= 100) {
+		puts("max health has been reached");
 		pet->health = maxHealthValue;
+	} else {
+		printf("You gave your pet a pill !\n");
+		puts("+30 to health");
+		pet->health += 30;
+		if (pet->health >= 100) {
+			pet->health = maxHealthValue;
+		} 
 	}
 }
 
 void refresh_status(Pet* pet, int *lastActionTime, bool * hasEaten) {
 	int timeDiff = 0;
-	timeDiff = (time(0) - *lastActionTime);
+	timeDiff = (time(0) - *lastActionTime) + 1;
+
+	// satiety increase
+
 	if (pet->satiety <= 0) {
 		pet->satiety = 0;
 	} else {
-		pet->satiety -= float(timeDiff * 2.2);
+		pet->satiety -= float(timeDiff * 1.5);
 	}
+
+	// poop increase
+
 	if (pet->poopLevel >= maxPoopLevel) {
 		pet->poopLevel = 100;
 	} else if (*hasEaten) {
 		pet->poopLevel += float(timeDiff * 1.7);
 	}
+
+	// hp loss
+
 	if (pet->satiety <= 0) {
-		pet->health -= float(timeDiff * 5);
+		pet->health -= float(timeDiff * 4);
+	}
+	if (pet->poopLevel >= 100) {
+		pet->health -= float(timeDiff * 4);
 	}
 }
